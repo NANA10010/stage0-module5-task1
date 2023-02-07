edc8e9e0a89cf93ec6369eaddc5605520e599b86
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-07 20:53:02 +0400

    Update ArrayTasks.java

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -116,13 +116,82 @@ public class ArrayTasks {
      * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]]
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
-     public int[][] sortRaggedArray(int[][] arr) {
+    static class MergeSort {
+        void merge(int arr[], int l, int m, int r) {
+            // Find sizes of two subarrays to be merged
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            /* Create temp arrays */
+            int L[] = new int[n1];
+            int R[] = new int[n2];
+
+            /*Copy data to temp arrays*/
+            for (int i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (int j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+
+            /* Merge the temp arrays */
+
+            // Initial indexes of first and second subarrays
+            int i = 0, j = 0;
+
+            // Initial index of merged subarray array
+            int k = l;
+            while (i < n1 && j < n2) {
+                if (L[i] <= R[j]) {
+                    arr[k] = L[i];
+                    i++;
+                } else {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            /* Copy remaining elements of L[] if any */
+            while (i < n1) {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            /* Copy remaining elements of R[] if any */
+            while (j < n2) {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        // Main function that sorts arr[l..r] using
+        // merge()
+        void sort(int arr[], int l, int r) {
+            if (l < r) {
+                // Find the middle point
+                int m = (l + r) / 2;
+
+                // Sort first and second halves
+                sort(arr, l, m);
+                sort(arr, m + 1, r);
+
+                // Merge the sorted halves
+                merge(arr, l, m, r);
+            }
+        }
+    }
+        /* A utility function to print array of size n */
+
         
+   public int[][] sortRaggedArray(int[][] arr) {
         int[][] sortedArr = new int[arr.length][];
-        
-        for (int i=0; i< arr.length; i++){
+        sortedArr = arr;
+        for (int i= arr.length-1; i>=0; i--){
             for (int j=0; j<arr[i].length; j++){
-                Arrays.sort(arr[i]);
+                MergeSort obj = new MergeSort();
+                obj.sort(arr[i], 0,arr[i].length-1);
                 sortedArr[i][j]=arr[i][j];
             }
         }
