9b89b5f8630e487133afe68dc2525778898a215c
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-11 19:52:21 +0400

    Update ArrayTasks.java

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -116,7 +116,7 @@ public class ArrayTasks {
      * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]]
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
-    static class MergeSort {
+    class MergeSort {
         void merge(int arr[], int l, int m, int r) {
             // Find sizes of two subarrays to be merged
             int n1 = m - l + 1;
